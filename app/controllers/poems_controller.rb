e31ddf0179610a0ccf13a48a1fb3e56241e2d246
satochikin <satochikin@sakikawasatoshi-no-MacBook-Air.local>
2017-07-31 15:58:40 +0900

    “test”　

--- app/controllers/poems_controller.rb
+++ app/controllers/poems_controller.rb
@@ -1,6 +1,11 @@
 class PoemsController < ApplicationController
   def index
     @poems =Poem.all
-    render json:@poems
+    render json: @poems
+  end
+
+  def show
+    @poem = Poem.find(params[:id]
+    render json: @porm
   end
 end
